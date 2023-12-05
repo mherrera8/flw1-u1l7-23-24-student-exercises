671c1473418b599e8a37317cb5eb9e68cc0eb7df
mherrera8 <147006626+mherrera8@users.noreply.github.com>
2023-12-05 00:24:06 +0000

    Learned more about using DOM and .style

--- stringInterpolation.js
+++ stringInterpolation.js
@@ -2,15 +2,18 @@
 let greeting = document.querySelector(".greeting");
 
 // 1. Create a variable called `firstName` and set the value to your name.
-
+let firstName = "Melania"
 
 // 2. Create a variable called `lastName` and set the value to your last name.
-
+let lastName = "Herrera"
 
 
 // 3. Create a variable called `greetingMessage`.
 // - Using string interpolation, create a messga that say "Hi, (your first name) (your last name)!"
+let greetingMessage = `Hi, ${firstName} ${lastName}!`;
 
+console.log(greetingMessage);
 
-
+  
 // 4. Set the innerHTML of the greeting div to the greeting message.
+greeting.innerHTML = greetingMessage;
